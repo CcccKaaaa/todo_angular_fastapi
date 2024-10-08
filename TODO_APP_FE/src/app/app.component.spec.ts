8201e2a5e5bd3302bebb189863a4303c3e710290
CcccKaaaa <90523619+CcccKaaaa@users.noreply.github.com>
2024-10-09 01:31:24 +0700

    Add docker compose

--- TODO_APP_FE/src/app/app.component.spec.ts
+++ TODO_APP_FE/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'todo_app' title`, () => {
+  it(`should have the 'TODO_APP_FE' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('todo_app');
+    expect(app.title).toEqual('TODO_APP_FE');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, todo_app');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, TODO_APP_FE');
   });
 });
